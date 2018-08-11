061a066a7607961da98c430ac61e829a847165cd
Luis Fernando Useche <Arqsunny@users.noreply.github.com>
2018-08-11 07:14:36 -0500

    upload1

--- data.js
+++ data.js
@@ -29,7 +29,14 @@ var APP_DATA = {
         "fov": 1.3401592523986738
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 2.893137626015836,
+          "pitch": -0.17653602083575315,
+          "title": "Rosalita's Cantina",
+          "text": "St. Louis, MO - 8-10-2018"
+        }
+      ]
     }
   ],
   "name": "Rosa",
